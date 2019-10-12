37c52bc45f264e80ee68099b82471060c90c0a8c
Hugo Trassoudaine <hugo.trsd@gmail.com>
2019-10-12 15:14:40 +0200

    Add Graph class

---
+++ src/core/graph.py
@@ -0,0 +1,43 @@
+from collections import namedtuple
+import json
+
+
+class Graph:
+    def __init__(self):
+        self.title = "untitled"
+        self.directed = False
+        self.vertices = []
+        self.edges = []
+
+    def saveToFile(self, filename):
+        filename = filename.replace(".json", "")  # Prevent doubled extension
+        with open(f"{filename}.json", "w") as file:
+            json.dump(self.__dict__, file, indent=2)
+
+    @staticmethod
+    def loadFromFile(filename: str):  # -> Graph
+        def dictToObj(data: dict):
+            g = Graph()
+            g.__dict__ = data.copy()  # That's crazy cool actually
+            return g
+
+        if not ".json" in filename:
+            filename += ".json"
+        with open(filename, "r") as file:
+            graph = json.load(file, object_hook=dictToObj)
+        return graph
+
+    def __eq__(self, obj):
+        if not isinstance(obj, Graph):
+            raise NotImplementedError
+
+        if self is obj:
+            return True
+
+        return self.__dict__ == obj.__dict__
+
+    def __ne__(self, obj):
+        return not self == obj
+
+    def __str__(self):
+        return f"{super().__str__()}: {str(self.__dict__)}"
